663daeb95086cf389501d13ba0f2c408a87e0ee6
walid-wagdy <37864584+walid-wagdy@users.noreply.github.com>
2026-01-12 16:07:41 +0200

    Push session, view, hashing, bootstrap, seeder, and Portfolio model

---
+++ config/hashing.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'driver' => env('CACHE_DRIVER', 'file'),
+];
---
+++ config/session.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'default' => env('SESSION_DRIVER', 'file'),
+    'lifetime' => env('SESSION_LIFETIME', 120),
+    'expire_on_close' => false,
+    'encrypt' => false,
+    'files' => storage_path('framework/sessions'),
+    'connection' => env('SESSION_CONNECTION'),
+    'table' => 'sessions',
+    'store' => env('SESSION_STORE'),
+    'lottery' => [2, 100],
+    'cookie' => env(
+        'SESSION_COOKIE',
+        Illuminate\Support\Str::slug(env('APP_NAME', 'portfolio'), '_').'_session'
+    ),
+    'path' => '/',
+    'domain' => env('SESSION_DOMAIN'),
+    'secure' => env('SESSION_SECURE_COOKIES'),
+    'http_only' => true,
+    'same_site' => 'lax',
+];
